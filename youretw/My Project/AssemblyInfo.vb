881aebac71dc0a71a29c6f7d4cf970efcfc1928c
liu-yun <liuyun.97@gmail.com>
2013-06-01 19:11:26 +0800

    UI improvements

--- youretw/My Project/AssemblyInfo.vb
+++ youretw/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' 方法是按如下所示使用“*”:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.2.0")> 
-<Assembly: AssemblyFileVersion("1.2.2.0")> 
+<Assembly: AssemblyVersion("1.2.3.0")> 
+<Assembly: AssemblyFileVersion("1.2.3.0")> 
